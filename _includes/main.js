8d9286f2555e304e7a2891110ed4955602ce9118
Jeff Martin <jeffmartin@gmail.com>
2012-08-30 00:19:08 -0700

    change footer bahvior again

--- _includes/main.js
+++ _includes/main.js
@@ -20,7 +20,7 @@ var Resize = (function() {
         content_height = content_height || $('#thepage').height() + $('.post_footer').height() + 5;
         var height_to_set = content_height > height ? content_height : height;
         $page_content.height(height_to_set);
-        $page_footer.addClass('grey');
+        $page_footer.addClass('grey').show();
     };
 
     return my;

daf8b43a95608bf61ac0b3db76333e074484503d
Jeff Martin <jeffmartin@gmail.com>
2012-08-30 21:41:28 -0700

    tweak style

--- _includes/main.js
+++ _includes/main.js
@@ -8,9 +8,30 @@ var Main = (function() {
 var Resize = (function() {
     var my = {};
     var content_height;
+    var $posts = $('#posts');
+
+    my.set_large_size = function() {
+        $posts = $posts.length ? $posts : $('#posts');
+        $posts.css({
+            'margin-left': '220px'
+        });
+    };
 
     my.on_resize = function(e) {
+        e = e || {};
+        $posts = $posts || $('#posts');
         //console.log(e);
+        var width = $(window).width();
+        if (width >= 1200) {
+            my.set_large_size();
+            return;
+        }
+
+        var margin = Math.max(220 - (1200 - width), 0);
+        $posts.css({
+            'margin-left': margin
+        });
+
     };
 
     my.set_doc_height = function(height) {
@@ -20,7 +41,7 @@ var Resize = (function() {
         content_height = content_height || $('#thepage').height() + $('.post_footer').height() + 5;
         var height_to_set = content_height > height ? content_height : height;
         $page_content.height(height_to_set);
-        $page_footer.addClass('grey').show();
+        $page_footer.addClass('grey_bg').show();
     };
 
     return my;
@@ -28,13 +49,14 @@ var Resize = (function() {
 
 function setup_events() {
     $(window).on('resize', function(e) {
+        Resize.on_resize();
         Resize.set_doc_height();
     });
 }
 
 $().ready(function() {
 
-
+    Resize.on_resize();
     if (!$('#posts img').length) {
         Resize.set_doc_height();
     }
